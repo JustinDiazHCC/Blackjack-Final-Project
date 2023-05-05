7d4aa96d449334a8acb72738c95b0e34cc777b86
JustinDiazHCC <justin.diaz@howardcc.edu>
2023-05-05 02:04:38 -0400

    Created About screen containing copyright/license information and a thank you message

--- src/main/java/edu/howardcc/javaii/AboutController.java
+++ src/main/java/edu/howardcc/javaii/AboutController.java
@@ -1,9 +1,26 @@
 package edu.howardcc.javaii;
 
-import java.io.IOException;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+
+import java.io.IOException;
 
 public class AboutController {
+
+    @FXML
+    private Label aboutText;
+
+    @FXML
+    private Button backButton;
+
+    @FXML
+    private ScrollPane scrollBox;
+
+    @FXML
+    private Label sourcesLabel;
 
     @FXML
     private void backToTitle() throws IOException {
